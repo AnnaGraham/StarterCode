7f507d2f775e42af2e15332d906c6cd503696fa8
Anna Graham <dramaqueen486@Annas-MacBook-Air.local>
2014-09-25 09:26:55 +0100

    Updated

--- TemperatureChanger.java
+++ TemperatureChanger.java
@@ -13,6 +13,20 @@ public class TemperatureChanger {
     // TODO - scan for input in the format 175F or 18C 
     // TODO - perform correct conversion
     // TODO - print out results to the screen
+Scanner scan = new Scanner(System.in);
+System.out.println("Please enter the temperature as an int");
+double temp= scan.nextDouble();
+System.out.println("Please enter whether it is farenheit or celcius using F or C");
+String fOrC = scan.next();
+if(fOrC.equals("C")){
+double tempResult = (double)temp*1.8 + 32;
+System.out.println(tempResult + "The the converted temperature");
+}else if(fOrC.equals("F")){
+double tempResult = (double)(temp - 32) / 1.8;
+System.out.println(tempResult + "The the converted temperature");
+}
+
+
   }
   
 }
